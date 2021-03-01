0509badadcd0b12c147b1e3b5fd65e777953bb7e
t3mp-0xCC <kurose_kiy@cma.bbiq.jp>
2021-03-01 23:40:15 +0900

    hoge

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,4 @@
-function fish_prompt --description 'Write out the prompt'
-    printf '\n%s \n%s' (set_color 366578)(__current_path) (set_color white)(printf '\xf0\x9f\x8d\xb5\x04< ')
-end
+
 
 function __current_path
   set pwd_result (pwd)
@@ -9,4 +7,16 @@ function __current_path
   else
 	  echo -n (set_color --bold 366578) $pwd_result (set_color normal)
   end
+end
+
+
+function fish_prompt --description 'Write out the prompt'
+    echo -n (__current_path)
+    echo -e ""
+    echo -e "\xf0\x9f\x8d\xb5\x04<"
+end
+
+function fish_right_prompt
+set_color brblue
+    date "+%H:%M:%S"
 end
--- fish/functions/fish_right_prompt.fish
+++
@@ -1,4 +0,0 @@
-function fish_right_prompt
-set_color brblue
-    date +"%H:%M:%S"
-end
