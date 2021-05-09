a64682ccbce05ae79d1e5a285fa6b7d50f9c9d0b
t3mp-0xCC <kurose_kiy@cma.bbiq.jp>
2021-05-09 15:54:22 +0900

    update .tmux.conf

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,9 +3,9 @@
 function __current_path
   set pwd_result (pwd)
   if test (string length $pwd_result) -gt (math $COLUMNS - 10)
-	  echo -n (set_color --bold 366578) (prompt_pwd) (set_color normal)
+	  echo -n (set_color --bold blue) (prompt_pwd) (set_color normal)
   else
-	  echo -n (set_color --bold 366578) $pwd_result (set_color normal)
+	  echo -n (set_color --bold blue) $pwd_result (set_color normal)
   end
 end
 
@@ -16,7 +16,3 @@ function fish_prompt --description 'Write out the prompt'
     echo -e "\xf0\x9f\x8d\xb5\x04<"
 end
 
-function fish_right_prompt
-set_color brblue
-    date "+%H:%M:%S"
-end
