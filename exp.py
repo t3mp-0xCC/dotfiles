7ca6e5fcc2f5c90485b8be40ac52724445bdb5fc
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-02-17 23:49:07 +0900

    update exp.py

--- exp.py
+++ exp.py
@@ -5,32 +5,34 @@ from pwn import *
 from sys import argv
 from time import sleep
 
-context.terminal = ['tmux', 'sp', '-h']
-context.log_level = "debug"
 
-chall = "./chall"
-#libc = ELF()
-elf = ELF(chall)
-context.binary = chall
-context.binary.checksec()
+if __name__ == '__main__':
+    context.terminal = ['tmux', 'sp', '-h']
+    context.log_level = "debug"
 
-if len(argv) >= 2 and argv[1] == "r":
-    p = remote("example.com", 4444)
-elif len(argv) >= 2 and argv[1] == "d":
-	cmd = """
-		b main
-		c
-	"""
-	p = gdb.debug(chall,cmd)
-else:
-    p = process(chall)
+    chall = "./chall"
+    #libc = ELF("./libc.so.6")
+    elf = ELF(chall)
+    context.binary = chall
+    context.binary.checksec()
+
+    if len(argv) >= 2 and argv[1] == "r":
+        p = remote("example.com", 4444)
+    elif len(argv) >= 2 and argv[1] == "d":
+    	cmd = """
+    		b main
+    		c
+    	"""
+    	p = gdb.debug(chall,cmd)
+    else:
+        p = process(chall)
 
 
-payload = b""
-payload +=b""
+    payload = b""
+    payload +=b""
 
-p.recvuntil("")
-sleep(0.5)
-p.sendline(payload)
+    p.recvuntil("")
+    sleep(0.5)
+    p.send(payload)
 
-p.interactive()
+    p.interactive()
