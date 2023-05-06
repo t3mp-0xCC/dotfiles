26799f0b18f0797bbdf6d1d43f0ceb87a1b18952
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2023-05-06 20:39:46 +0900

    neovim BIG UPDATE

--- nvim/.config/nvim/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Downloading dein.vim installer..."
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-echo "Run install script"
-sh ./installer.sh $HOME/.config/nvim/dein
-rm ./installer.sh
-echo "All Done !"
