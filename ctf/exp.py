37de80a0a358f360ed681682398d02cf2b1c7a94
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2023-10-03 20:01:07 +0900

    improve: new exploit template

--- ctf/exp.py
+++ ctf/exp.py
@@ -6,33 +6,44 @@ from sys import argv
 from time import sleep
 
 
-if __name__ == '__main__':
-    context.terminal = ['tmux', 'sp', '-h']
-    context.log_level = "debug"
+context.terminal = ['tmux', 'sp', '-h']
+context.log_level = "debug"
 
-    chall = "./chall"
-    #libc = ELF("./libc.so.6")
-    elf = ELF(chall)
-    context.binary = chall
-    context.binary.checksec()
+chall = "./chall"
+#libc = ELF("./libc.so.6")
+elf = ELF(chall)
+context.binary = chall
+context.binary.checksec()
 
-    if len(argv) >= 2 and argv[1] == "r":
-        p = remote("example.com", 4444)
-    elif len(argv) >= 2 and argv[1] == "d":
-    	cmd = """
-    		b main
-    		c
-    	"""
-    	p = gdb.debug(chall,cmd)
+if len(argv) >= 2 and argv[1] == "r":
+    p = remote("example.com", 4444)
+elif len(argv) >= 2 and argv[1] == "d":
+	cmd = """
+		b main
+		c
+	"""
+	p = gdb.debug(chall,cmd)
+else:
+    p = process(chall)
+
+def conv(x):
+    if type(x) is bytes:
+        return x
     else:
-        p = process(chall)
+        return x.encode()
+
+def sl(x):
+    p.sendline(conv(x))
+
+def sla(delim, data):
+    p.sendlineafter(conv(delim), conv(data))
 
 
-    payload = b""
-    payload +=b""
+payload = b""
+payload +=b""
 
-    p.recvuntil("")
-    sleep(0.5)
-    p.send(payload)
+p.recvuntil("")
+sleep(0.5)
+p.send(payload)
 
-    p.interactive()
+p.interactive()
